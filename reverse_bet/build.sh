31340957eedea5df7266f91cbaebdd5504a889af
Maintain <duonghb53@users.noreply.github.com>
2022-03-06 23:21:15 +0700

    Update build.sh

--- reverse_bet/build.sh
+++ reverse_bet/build.sh
@@ -1,2 +1,2 @@
-cargo build --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/reverse_bet.wasm ./res/reverse_bet.wasm
